65fb1b321bb7551571aa4cb66d3bf067d0667be5
OOwinoK <oyugi.owino.k@gmail.com>
2024-12-11 14:00:28 +0300

    block chain data

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module expert-disco
 
 go 1.23.2
+
+require github.com/coinpaprika/coinpaprika-api-go-client v2.4.0+incompatible
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/coinpaprika/coinpaprika-api-go-client/coinpaprika"
+)
 
 func main() {
-	fmt.Println("Boilerplate")
+	paprikaClient := coinpaprika.NewClient(nil)
 
+	tickers, err := paprikaClient.Tickers.List(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, t := range tickers {
+		if t.Name == nil || t.Symbol == nil || t.Rank == nil {
+			continue
+		}
+
+		fmt.Println("Name:", *t.Name)
+		fmt.Println("Symbol:", *t.Symbol)
+		fmt.Println("Rank:", *t.Rank)
+		fmt.Println("----")
+	}
 }
